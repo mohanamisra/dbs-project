666684600932a75eb81848b2445976dd4f68a016
Mohana Misra <iammohanamisra@gmail.com>
2024-04-12 01:40:59 +0530

    Landing page designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
       <>
+          <Navbar/>
+          <Hero/>
+          <Footer/>
       </>
   )
 }
