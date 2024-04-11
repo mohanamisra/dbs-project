1fe5d74fdbec2624409a924d92bad311b61088b9
Mohana Misra <iammohanamisra@gmail.com>
2024-04-12 00:31:00 +0530

    Basic footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,12 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div className = 'footer-container'>
-            <div className = 'disclaimer'>
-                Disclaimer: Add Some Relevant Disclaimer.
-            </div>
-            <div className = 'attributes'>
+        <div className='footer-container'>
+            <div className='attributes'>
                 CryptoNivesh 2024
+            </div>
+            <div className='disclaimer'>
+                Disclaimer: Add Some Relevant Disclaimer.
             </div>
         </div>
     )
