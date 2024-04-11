fcea69afee0db1b44d3ac5d9e22b996ab25e9b77
Mohana Misra <iammohanamisra@gmail.com>
2024-04-12 01:40:46 +0530

    Added footer disclaimer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,9 @@ function Footer() {
                 CryptoNivesh 2024
             </div>
             <div className='disclaimer'>
-                Disclaimer: Add Some Relevant Disclaimer.
+                Disclaimer: Cryptocurrency investments carry risks. Past performance doesn't guarantee future results.
+                We are not responsible for market inconsistencies and/or inaccurate performance of the APIs used
+                to build this web application. Always consult financial experts before making a monetary investment.
             </div>
         </div>
     )
