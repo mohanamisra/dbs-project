873efe0fb0dc6b58880af1d5e9bf9cc6e1549915
Mohana Misra <iammohanamisra@gmail.com>
2024-04-11 23:35:01 +0530

    Basic footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className = 'footer-container'>
+            <div className = 'disclaimer'>
+                Disclaimer: Add Some Relevant Disclaimer.
+            </div>
+            <div className = 'attributes'>
+                CryptoNivesh 2024
+            </div>
+        </div>
+    )
+}
+
+export default Footer
