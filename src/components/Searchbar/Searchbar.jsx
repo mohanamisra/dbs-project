23408fef1ae7e198d45cc65513a9a328f6217659
Mohana Misra <iammohanamisra@gmail.com>
2024-04-13 18:07:15 +0530

    Integrating polygon client

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import 
+import React, {useState} from 'react';
+import {FaSearch} from "react-icons/fa";
+import { restClient } from '@polygon.io/client-js';
 
 function Searchbar() {
+    const rest = restClient('Ymu2WtW7IYPjySqMRwIzYP622FQKbvMp');
+    const [input, setInput] = useState('');
+
+    const handleInputChange = (e) => {
+        let value = e.target.value;
+        setInput(value);
+    }
+
+    const handleSearchClick = () => {
+        // fetchData(input);
+        rest.crypto.rsi(input).then((data) => {
+            console.log(data.results.values[0].value);
+        }).catch(e => {
+            console.error('An error happened:', e);
+        });
+        rest.crypto.previousClose(input).then((data) => {
+            console.log(`Closing Price = $${data.results[0].c} Opening Price = $${data.results[0].o}`);
+        }).catch(e => {
+            console.error('An error happened:', e);
+        });
+    }
+
     return (
         <div className = 'input-wrapper'>
-
+            <input placeholder = 'Enter your crypto ticker...' onChange = {handleInputChange}/>
+            <button className = 'search-button' onClick={handleSearchClick}>
+                <FaSearch className = 'search-icon'/>
+            </button>
         </div>
     );
 }
 
-export default Searchbar;
+export default Searchbar
